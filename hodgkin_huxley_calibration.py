f1f92399a2e4250c93b9a2a26238f73a05d484bc
thiagoburghi <67546053+thiagoburghi@users.noreply.github.com>
2021-02-07 21:17:31 +0000

    Thiago (#1)
    * Update hodgkin_huxley_calibration.py
    
    * Update hodgkin_huxley_calibration.py
    
    * Update hodgkin_huxley_calibration.py
    
    * Update hodgkin_huxley_calibration.py
    
    * Update hodgkin_huxley_calibration.py
    
    * Update hodgkin_huxley_calibration.py
    
    * Update hodgkin_huxley_calibration.py

--- hodgkin_huxley_calibration.py
+++ hodgkin_huxley_calibration.py
@@ -1,61 +1,130 @@
 # Add uncertainty to Hodgkin-Huxley parameters, try 'recalibrating' by
 # adjusting the maximal conductance parameters to keep onset of spiking
 # unperturbed
-
+#%%
+from abc import ABC, abstractmethod  # for abstract classes
 import numpy as np
 from numpy import exp
 
 import matplotlib.pyplot as plt
 from scipy.integrate import solve_ivp
 
-# Offsets to alpha/beta functions, generate randomly
-mag = 10
-dValpham = mag*np.random.normal(0,1)
-dVbetam = mag*np.random.normal(0,1)
-dValphah = mag*np.random.normal(0,1)
-dVbetah = mag*np.random.normal(0,1)
-dValphan = mag*np.random.normal(0,1)
-dVbetan = mag*np.random.normal(0,1)
+class HHKinetics(ABC):
+    """
+    HH-type (alpha-beta) kinetics abstract class.
+    Has to be implemented by an activation or inactivation subclass.
+    """
+    @abstractmethod
+    def alpha(self,V):
+        pass
 
-def alpha_m(V):
-    V = V + dValpham # add offset due to uncertainty
-    return 0.1 * (25 - V) / (exp((25 - V) / 10) - 1)
+    @abstractmethod
+    def beta(self,V):
+        pass
 
-def beta_m(V):
-    V = V + dVbetam # add offset due to uncertainty
-    return 4 * exp(-V / 18)
+    def inf(self,V):
+        return self.alpha(V) / (self.alpha(V) + self.beta(V))
 
-def alpha_h(V):
-    V = V + dValphah # add offset due to uncertainty
-    return 0.07 * exp(-V / 20)
+    def tau(self, V):
+        return 1 / (self.alpha(V) + self.beta(V))
 
-def beta_h(V):
-    V = V + dVbetah # add offset due to uncertainty
-    return 1 / (exp((30 - V) / 10) + 1)
+    def diff(self,V,x):
+        return self.alpha(V)*(1 - x) - self.beta(V)*x
 
-def alpha_n(V):
-    V = V + dValphan # add offset due to uncertainty
-    return 0.01 * (10 - V) / (exp((10 - V)/10) - 1)
+class HHActivation(HHKinetics):
+    """
+    HH-type (alpha-beta) activation gating variable kinetics.
+    """
+    def __init__(self, aVh, aA, aK, bVh, bA, bK):
+        self.aVh = aVh
+        self.aA = aA
+        self.aK = aK
+        self.bVh = bVh
+        self.bA = bA
+        self.bK = bK
 
-def beta_n(V):
-    V = V + dVbetan # add offset due to uncertainty
-    return 0.125 * exp(-V / 80)
+    def alpha(self,V):
+        A = self.aA
+        K = self.aK
+        Vh = self.aVh
+        a =	np.zeros(np.size(V))
+        V = np.array(V)
+        a[V!=Vh] = A * (Vh - V[V!=Vh]) / (exp((Vh - V[V!=Vh]) / K) - 1)
+        a[V==Vh] = A*K
+        return a
 
-def x_inf(V, alpha, beta):
-    return alpha(V) / (alpha(V) + beta(V))
+    def beta(self,V):
+        return self.bA * exp((self.bVh - V) / self.bK)
 
-def tau(V, alpha, beta):
-    return 1 / (alpha(V) + beta(V))
+class HHInactivation(HHKinetics):
+    """
+    HH-type (alpha-beta) inactivation gating variable kinetics. 
+    """
+    def __init__(self, aVh, aA, aK, bVh, bA, bK):
+        self.aVh = aVh
+        self.aA = aA
+        self.aK = aK
+        self.bVh = bVh
+        self.bA = bA
+        self.bK = bK
 
-# Nernst potentials and maximal conductances for potassium, sodium and leak
+    def alpha(self,V):
+        return self.aA * exp((self.aVh - V)/ self.aK)
+
+    def beta(self,V):
+        return self.bA / (exp((self.bVh - V) / self.bK) + 1)
+
+#%% Plot 'IV' curves
+V = np.arange(-20,130,0.5)
+
+# HH Nernst potentials and maximal conductances for potassium, sodium and leak
 gk = 36
 gna = 120
 gl = 0.3
-
 Ek = -12
 Ena = 120
 El = 10.6
 
+# Nominal HH kinetics
+m_HH = HHActivation(25, 0.1, 10, 0, 4, 18)
+h_HH = HHInactivation(0, 0.07, 20, 30, 1, 10)
+n_HH = HHActivation(10, 0.01, 10, 0, 0.125, 80)
+
+# IV curves for nominal HH model
+Ifast_HH = gl*(V - El) + gna*m_HH.inf(V)**3*h_HH.inf(V)*(V - Ena)
+Islow_HH = Ifast_HH + gk*n_HH.inf(V)**4*(V - Ek)
+
+# Offsets to perturb alpha/beta HH functions, generate randomly
+mag = 10
+Valpham = 25 + mag*np.random.normal(0,1)
+Vbetam = 0 + mag*np.random.normal(0,1)
+Valphah = 0 + mag*np.random.normal(0,1)
+Vbetah = 30 + mag*np.random.normal(0,1)
+Valphan = 10 + mag*np.random.normal(0,1)
+Vbetan = 0 + mag*np.random.normal(0,1)
+
+# Perturbed HH kinetics
+m_P = HHActivation(Valpham, 0.1, 10, Vbetam, 4, 18)
+h_P = HHInactivation(Valphah, 0.07, 20, Vbetah, 1, 10)
+n_P = HHActivation(Valphan, 0.01, 10, Vbetan, 0.125, 80)
+
+# IV basis functions for Perturbed HH model
+Na_bf = m_P.inf(V)**3*h_P.inf(V)*(V - Ena)
+K_bf = n_P.inf(V)**4*(V - Ek)
+L_bf = (V - El)
+
+# IV curves for nominal HH model
+Ifast = gl*L_bf + gna*Na_bf
+Islow = Ifast + gk*K_bf
+
+plt.figure()
+plt.plot(V, Ifast_HH, V, Ifast)
+plt.legend(['HH','perturbed HH'])
+plt.figure()
+plt.plot(V, Islow_HH, V, Islow)
+plt.legend(['HH','perturbed HH'])
+
+#%% Simulation
 # Define length of the simulation (in ms)
 T = 500
 
@@ -70,49 +139,37 @@ def ramp(t):
     I = (t>=0)*I1 + (t/T)*(I2 - I1)
     return I
 
-# Define x_inf functions
-m_inf = lambda V: x_inf(V, alpha_m, beta_m)
-h_inf = lambda V: x_inf(V, alpha_h, beta_h)
-n_inf = lambda V: x_inf(V, alpha_n, beta_n)
-
-# Define tau functions
-tau_m = lambda V: tau(V, alpha_m, beta_m)
-tau_h = lambda V: tau(V, alpha_h, beta_h)
-tau_n = lambda V: tau(V, alpha_n, beta_n)
-
-def odesys(t, y):
+def odesys(t, y, gates):
     V, m, h, n = y
     
     I = I0
     #I = ramp(t)
     
+    M = gates[0]
+    H = gates[1]
+    N = gates[2]
+
     dV = -gl*(V - El) - gna*m**3*h*(V - Ena) - gk*n**4*(V - Ek) + I
-    dm = alpha_m(V)*(1 - m) - beta_m(V)*m
-    dh = alpha_h(V)*(1 - h) - beta_h(V)*h
-    dn = alpha_n(V)*(1 - n) - beta_n(V)*n
+    dm = M.diff(V,m)
+    dh = H.diff(V,h)
+    dn = N.diff(V,n)
     return [dV, dm, dh, dn]
 
 trange = (0, T)
 
 # Initial state y = [V0, m0, h0, n0], set at Vrest = 0
 V0 = 0.001
-y0 = [V0, m_inf(V0), h_inf(V0), n_inf(V0)]
+y0 = [V0, m_HH.inf(V0), h_HH.inf(V0), n_HH.inf(V0)]
 
-sol = solve_ivp(odesys, trange, y0)
+gates_HH = [m_HH,h_HH,n_HH]
+gates_P = [m_P,h_P,n_P]
+
+sol_HH = solve_ivp(lambda t,y : odesys(t,y,gates_HH), trange, y0)
+sol_P = solve_ivp(lambda t,y : odesys(t,y,gates_P), trange, y0)
 
 # Plot the simulation
 plt.figure()
-plt.plot(sol.t, sol.y[0])
+plt.plot(sol_HH.t, sol_HH.y[0],sol_P.t, sol_P.y[0])
+plt.legend(['HH','perturbed HH'])
 plt.figure()
-plt.plot(sol.t, ramp(sol.t))
-
-# Plot 'IV' curves
-# Note: Division by 0 in alpha_m(V) and alpha_n(V)
-V = np.arange(-20,130,0.5)
-Ifast = gl*(V - El) + gna*m_inf(V)**3*h_inf(V)*(V - Ena)
-Islow = Ifast + gk*n_inf(V)**4*(V - Ek)
-
-plt.figure()
-plt.plot(V, Ifast)
-plt.figure()
-plt.plot(V, Islow)
+plt.plot(sol_HH.t, ramp(sol_HH.t))
