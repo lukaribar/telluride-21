d89c71f894b5154407a011985a0b47e38a7c6243
Luka <lukaribar.mg@gmail.com>
2021-03-16 19:23:34 +0000

    Added inputs

--- gui_utilities.py
+++ gui_utilities.py
@@ -10,17 +10,13 @@ class GUI:
     
     args:
         system: Neuron or Network class to be simulated
-    
-    kwargs:
-        vmin, vmax, dv: voltage range of the IV curves
-        i0: initial applied current
     """  
     def __init__(self, system):              
         self.system = system # associate GUI with a neuron or a network
         
         # Initial parameters
         self.i0 = 0
-        self.t_start_list = [30, 50, 70]
+        self.t_start_list = [10, 45, 80]
         self.tau_list = [1, 1, 1]
         self.mag_list = [1, 2, 3]
         self.alpha_list = []
@@ -36,37 +32,66 @@ class GUI:
         self.i_min = -1
         self.i_max = 10
         self.t_max = 100
+        self.t = np.arange(0, self.t_max, 0.1)
         
         # Create empty plot
         plt.close("all")
         self.fig = plt.figure()
         
-        # Add simulation plot
+        # Add voltage plot
         self.ax_out = self.fig.add_subplot(2, 1, 1)
-        self.ax_out.set_position([0.1, 0.45, 0.8, 0.2])
+        self.ax_out.set_position([0.1, 0.75, 0.8, 0.2])
         self.ax_out.set_xlim((0, self.t_max))
         self.ax_out.set_ylim((self.V_min, self.V_max))
-        self.ax_out.set_xlabel('Time')
         self.ax_out.set_ylabel('V')
         
-        # Add input current plot
+        # Add Iapp plot
         self.ax_in = self.fig.add_subplot(2, 1, 2)
-        self.ax_in.set_position([0.1, 0.25, 0.8, 0.2])
-        self.ax_in.set_ylim((self.i_min, self.i_max))
-        self.ax_out.set_xlabel('Time')
-        self.ax_out.set_ylabel('Iapp')
-    
+        self.ax_in.set_position([0.1, 0.45, 0.8, 0.2])
+        self.update_input()
+        
+        # Add sliders for magnitudes
+        self.s1 = self.add_slider("Input 1", [0.1, 0.3, 0.3, 0.03], 0, 5,
+                                  self.mag_list[0], self.alpha_list[0].set_mag)
+        self.s2 = self.add_slider("Input 2", [0.1, 0.25, 0.3, 0.03], 0, 5,
+                                  self.mag_list[1], self.alpha_list[1].set_mag)
+        self.s3 = self.add_slider("Input 3", [0.1, 0.2, 0.3, 0.03], 0, 5,
+                                  self.mag_list[2], self.alpha_list[2].set_mag)
+        
+        # Add sliders for taus
+        self.s4 = self.add_slider("", [0.6, 0.3, 0.3, 0.03], 0.1, 5,
+                                  self.tau_list[0], self.alpha_list[0].set_tau)
+        self.s5 = self.add_slider("", [0.6, 0.25, 0.3, 0.03], 0.1, 5,
+                                  self.tau_list[1], self.alpha_list[1].set_tau)
+        self.s6 = self.add_slider("", [0.6, 0.2, 0.3, 0.03], 0.1, 5,
+                                  self.tau_list[2], self.alpha_list[2].set_tau)
+        
     class Alpha:
         def __init__(self, t_start, tau, mag):
             self.t_start = t_start
             self.tau = tau
             self.mag = mag
+        
+        def set_mag(self, mag):
+            self.mag = mag
             
+        def set_tau(self, tau):
+            self.tau = tau
+        
         def out(self, t):
             t1 = (t - self.t_start) / self.tau
-            I_out = (t1 <= self.t_start) * 0
-            + (t1 > self.t_start) * self.mag * t1*np.exp(1 - t1)
+            t2 = (t1>0) * t1
+            I_out = self.mag * t2*np.exp(1 - t2)
             return I_out    
+    
+    def update_input(self):
+        I = self.i_app(self.t)
+        self.ax_in.cla()
+        self.ax_in.set_xlim((0, self.t_max))
+        self.ax_in.set_ylim((self.i_min, self.i_max))
+        self.ax_in.set_xlabel('Time')
+        self.ax_in.set_ylabel('Iapp')
+        self.ax_in.plot(self.t, I)
     
     def i_app(self, t):
         I_out = self.i0
@@ -76,7 +101,7 @@ class GUI:
             
     def update_val(self, val, update_method):
         update_method(val)
-        self.update_IV_curves()
+        self.update_input()
         
     def add_slider(self, name, coords, val_min, val_max, val_init,
                    update_method):
@@ -93,4 +118,5 @@ class GUI:
         
     def add_label(self, x, y, text):
         plt.figtext(x, y, text, horizontalalignment = 'center')
-         
+
+    
