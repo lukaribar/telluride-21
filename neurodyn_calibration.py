correct scaling code

neurodyn_calibration.py
@@ -5,19 +5,20 @@ import matplotlib.pyplot as plt
 from scipy.optimize import nnls, minimize, Bounds
 
 ND = NeuroDynModel()
-kappa,C,C_m,Vt,I_tau,I_ref,V_ref = ND.get_default_rate_pars()
+kappa,C,C_ND,Vt,I_tau,I_ref,V_ref = ND.get_default_rate_pars()
 kappa = 0.7
 
-# Voltage scaling:  V_ND = V_HH * scl_v, with V_HH in [mV]
-scl_v = 2.2/1e3
-# Time scaling:     t_ND = t_HH * scl_t, with t_HH in [ms]
-C_HH = 1    # [\mu F]
-s = 1e3
-scl_t = C_m/C_HH*s
+# Important: all scaling variables assume that HH is already written in SI units
+# Voltage scaling:
+scl_v = 2.2
+# Time scaling:
+C_HH = 1e-6
+s = 1/4*1e6
+scl_t = s*C_ND/C_HH
 
 # Create a HH that is scaled in voltage so that Neurodyn is able to reproduce 
 # the geometry of its dynamics
-HH = HHModel(scl=scl_v)
+HH = HHModel(scl=scl_v*1e-3)
 X = [HH.m,HH.h,HH.n]
 
 plots = False
@@ -137,8 +138,8 @@ Ib = []
 for i,x in enumerate(X):
     # Fit and recover alpha and beta based on linear model
     c_a,c_b,A_alpha,A_beta = lsqfit(x,Vrange,Vhalf,kappa,Vt)
-    i_a = c_a * C * Vt / scl_t
-    i_b = c_b * C * Vt / scl_t
+    i_a = c_a * C * Vt * 1000 / scl_t
+    i_b = c_b * C * Vt * 1000 / scl_t
     Ib.append([i_a, i_b])
     alpha = np.dot(A_alpha,c_a)
     beta = np.dot(A_beta,c_b)
@@ -168,9 +169,9 @@ for i,x in enumerate(X):
         plt.legend()
 
 #%%
-ND = NeuroDynModel(np.array([120,36,0.3])/s, [HH.Ena, HH.Ek, HH.El], Ib, Vmean+3.5*Vstep, Vmean-3.5*Vstep)
+ND = NeuroDynModel(np.array([120,36,0.3])*1e-3/s,np.array([120,-12,10.6])*1e-3*scl_v, Ib, Vmean+3.5*Vstep, Vmean-3.5*Vstep)
 
-I0 = scl_v*1e-3*10              # scaling??
+I0 = (10*1e-6)*scl_v/s              # scaling??
 Iapp = lambda t : I0
 def Ibump(t):
     if t < 0.004: